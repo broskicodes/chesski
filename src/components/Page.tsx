eb90181f7eb905f2283ad937523a226a1140b406
nibbus <nibbus0x@protonmail.com>
2023-08-19 12:44:55 -0400

    add vercel analytics

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,11 +2,13 @@ import { PropsWithChildren } from "react";
 import { NotificationProvider } from "../providers/NotificationProvider";
 import { SessionProvider } from "../providers/OddSessionProvider";
 import { Notifications } from "./notifications/Notifications";
+import { Analytics } from "@vercel/analytics/react";
 
 export const Page = ({ children }: PropsWithChildren) => {
   return (
     <div className={"container mx-auto h-screen"}>
       <title>Chesski</title>
+      <Analytics />
       <NotificationProvider>
         <SessionProvider>
           <Notifications />
